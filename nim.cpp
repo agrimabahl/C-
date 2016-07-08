4c6c4702ebcec17a27048895a9de1db58d8925f8
agrimabahl <agrimabahl@users.noreply.github.com>
2016-07-07 20:25:55 -0700

    Create nim.cpp

---
+++ nim.cpp
@@ -0,0 +1,15 @@
+#Game of Nim. 
+/*There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. 
+The one who removes the last stone will be the winner.*/
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n==1)
+        return 1;
+        else if !(n%4)
+        return 0;
+        else
+        return 1;
+    }
+};
