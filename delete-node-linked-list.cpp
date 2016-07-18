25b12963f37ca377b0afb0b4bb61055be7397d54
agrimabahl <agrimabahl@users.noreply.github.com>
2016-07-17 23:36:24 -0700

    Create delete-node-linked-list.cpp

---
+++ delete-node-linked-list.cpp
@@ -0,0 +1,22 @@
+//Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
+//the linked list should become 1 -> 2 -> 4 after calling your function.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node==NULL)
+        return;
+       if(node->next)
+       {
+           node->val=node->next->val;
+           node->next=node->next->next;
+       }
+    }
+};
